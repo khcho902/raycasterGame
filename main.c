d8d2536072892eb6da261880b960b84bc704eaae
khcho902 <khcho902@gmail.com>
2020-08-07 12:39:31 +0900

    gray window

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <GLUT/glut.h>
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+void init()
+{
+	glClearColor(0.3, 0.3, 0.3, 0);
+	gluOrtho2D(0, 1024, 512, 0);
+}
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(1024, 512);
+	glutCreateWindow("YouTube - 3Dsage");
+	init();
+	glutDisplayFunc(display);
+	glutMainLoop();
+}
