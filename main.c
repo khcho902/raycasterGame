9f0ff9ca8ce1ab8287364e02d319e359c5e1ef9d
khcho902 <khcho902@gmail.com>
2020-08-07 13:02:20 +0900

    add player

--- main.c
+++ main.c
@@ -2,9 +2,21 @@
 #include <stdlib.h>
 #include <GLUT/glut.h>
 
+float px,py; //player position
+
+void drawPlayer()
+{
+	glColor3f(1, 1, 0);
+	glPointSize(8);
+	glBegin(GL_POINTS);
+	glVertex2i(px, py);
+	glEnd();
+}
+
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	drawPlayer();
 	glutSwapBuffers();
 }
 
@@ -12,6 +24,7 @@ void init()
 {
 	glClearColor(0.3, 0.3, 0.3, 0);
 	gluOrtho2D(0, 1024, 512, 0);
+	px=300; py=300;
 }
 
 int main(int argc, char* argv[])
